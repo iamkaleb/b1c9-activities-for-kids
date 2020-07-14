9b99e9f9feeb903a52283b65f41e44876b7276ec
iamkaleb <kalebdmoran@gmail.com>
2020-07-14 14:34:54 -0500

    initial commit

---
+++ activities-for-kids.py
@@ -0,0 +1,23 @@
+def run(*kids):
+    for kid in kids:
+        print(f'{kid} ran around like a chicken with its head cut off.')
+
+def swing(*kids):
+    for kid in kids:
+        print(f'{kid} jumped out of the swing.')
+
+def slide(*kids):
+    for kid in kids:
+        print(f'{kid} went down the slide.')
+
+def hide_and_seek(*kids):
+    for kid in kids:
+        print(f'{kid} played hide and seek by themselves.')
+
+run('Pam', 'Sam', 'Andrea', 'Will')
+swing('Marybeth', 'Ariel', 'Kevin', 'Courtney')
+slide('Mike', 'Jack', 'Jennifer', 'Earl')
+hide_and_seek('Henry', 'Heather', 'Hayley', 'Hugh')
+
+
+
